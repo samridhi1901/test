1fe135dbf4377285ae488e198aec072e4557b51e
samridhi singh <gurjarsam6@gmail.com>
2025-01-31 12:14:31 +0530

    substring

---
+++ substring.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class substring{
+	public static void main (String[]args){
+	str1 = "abcdefabc";
+	str2 = "abc";
+	int count =0;
+	str1.toCharArray;
+	str2.toCharArray;
+	for(int i=0;i<str1.length;i++){
+	for(int j=0;j<str2.length;j++){
+			if (str1[i] == str2[j]){
+				i++;
+				j++;}
+	}
+		
+	count++;
+	}
+	}
+}	
